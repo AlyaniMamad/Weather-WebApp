ace5278b2cacd2c37b4fc1101ac0ebfd613470eb
Alyani Mamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-06-20 16:27:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alyani Mamad
         </a>
       </header>
     </div>
